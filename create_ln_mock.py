a599f9e5b9502afc6e2505f9d307e9206e65cb1d
Ivan Espinoza <ivanespinoza@calvin.physics.arizona.edu>
2025-06-24 18:10:41 -0700

    Added a functioning creator of LN mock data, does not include shape noise yet

--- create_ln_mock.py
+++ create_ln_mock.py
@@ -12,35 +12,31 @@ torch.set_num_threads(8)
 configfile     = sys.argv[1]
 config         = KarmmaConfig(configfile)
 
-try:
-    lowpass_filter = bool(sys.argv[2])
-    if(lowpass_filter):
-        print("CREATING LOW-PASS FILTERED MAPS!")
-except:
-    lowpass_filter = False
-
 nside    = config.analysis['nside']
 nbins    = config.analysis['nbins']
 gen_lmax = 3 * nside - 1
 lmax     = 2 * nside - 1
 
-# N_Z_BINS = config.analysis['nbins']
+N_Z_BINS = config.analysis['nbins']
 shift    = config.analysis['shift']
 vargauss = config.analysis['vargauss']
 
-nz_data  = config.analysis['nz']
 sigma_e  = config.analysis['sigma_e']
 
-cl = config.analysis['cl'][:,:,:(gen_lmax + 1)]
-cl_emu = None
+cl = np.zeros((N_Z_BINS,N_Z_BINS,gen_lmax+1))
 
+thetafid = torch.tensor(config.thetafid,dtype=torch.double)
 #============================================================
 print("Initializing sampler....")
 tmp = np.zeros((nbins,hp.nside2npix(nside)))
-tmp = KarmmaSampler(tmp, tmp, tmp, tmp, cl, shift, vargauss, cl_emu, lmax, gen_lmax)
+tmp = KarmmaSampler(tmp, tmp, tmp, tmp, cl, shift, vargauss, lmax, gen_lmax,
+                        shift_file=config.shift_file,mean_g_file=config.mean_g_file,ycl_file=config.y_cl_file)
 print("Done initializing sampler....")
 
 ell, emm = hp.Alm.getlm(gen_lmax)
+ycl    = tmp.cl_emu.predict(thetafid).reshape((1, N_Z_BINS, N_Z_BINS, -1))[0].numpy()
+mean_g = tmp.mean_g_emu.predict(thetafid)[0].numpy()
+shift  = tmp.shift_emu.predict(thetafid)[0].numpy()
 
 def eigvec_matmul(A, x):
     y = np.zeros_like(x)
@@ -84,7 +80,7 @@ def generate_xlm():
 
 def generate_mock_y_lm():
     xlm = generate_xlm()
-    return apply_cl(xlm, tmp.y_cl)
+    return apply_cl(xlm, ycl)
 
 mask    = hp.fitsfunc.read_map(config.maskfile)
 boolean_mask = mask.astype(bool)
@@ -97,15 +93,19 @@ def get_y_maps():
         y_maps.append(y_map)    
     return np.array(y_maps)    
 
+def low_pass_filter(map,nside):
+    map_lm = hp.map2alm(map,lmax=3*nside-1)
+    ell,emm = hp.Alm.getlm(3*nside-1)
+    map_lm[ell>2*nside]=0.+0.*1j
+    return hp.alm2map(map_lm,nside=nside)
+
 def get_LN_shear(y_maps):
     g1_list = []
     g2_list = []
     k_list = []
     for i in range(nbins):
-        k = np.exp(y_maps[i] + tmp.mu[i]) - shift[i]
-        if(lowpass_filter):
-            lowpass_ell_filter = (ell < lmax)
-            k = get_filtered_map(k, lowpass_ell_filter, nside)
+        k_nf = np.exp(y_maps[i] + mean_g[i]) - shift[i]
+        k = low_pass_filter(k_nf, nside)
         k_list.append(k)
         g1, g2 = trf.conv2shear(torch.tensor(k), lmax)
         g1 = g1.numpy() * mask
@@ -118,8 +118,25 @@ def get_LN_shear(y_maps):
     k_arr  = np.array(k_list)
     
     return g1, g2, k_arr  
-        
+
+def save_datafile(N,g1_obs,g2_obs,mask,outpath=config.datafile):
+    hf = h5.File(outpath, 'w')
+    hf.create_dataset('N',data      = N)
+    hf.create_dataset('g1_obs',data = g1_obs)
+    hf.create_dataset('g2_obs',data = g2_obs)
+    hf.create_dataset('mask',data   = mask)
+    hf.close()
+
 y_maps            = get_y_maps()
 g1, g2, k_arr     = get_LN_shear(y_maps)
-g1_obs, g2_obs, N = get_mock_data(nside, nbins, [config.analysis['nbar'], config.analysis['sigma_e'], g1, g2, mask])        
-save_datafile(config.datafile, g1_obs, g2_obs, k_arr, N, mask)
+
+y_maps            = get_y_maps()
+g1, g2, k_arr     = get_LN_shear(y_maps)
+#==========Figure this out!
+g1_obs = g1 
+g2_obs = g2   
+with h5.File('/home/ivanespinoza/KaRMMa/KaRMMa/data/des_y3/desy3_shear_data.h5', 'r') as f:
+    N    = f['N'][()]
+#g1_obs, g2_obs, N = get_mock_data(nside, nbins, [config.analysis['nbar'], config.analysis['sigma_e'], g1, g2, mask])        
+#==========Figure this out!
+save_datafile(N,g1_obs,g2_obs,mask)
